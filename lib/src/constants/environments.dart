e8b35d5e3f47af1b8b093bfad6ce16d20015949c
Denzel Code <denzelcodedev@gmail.com>
2021-07-20 01:56:38 -0400

    fix: logout behavior updated

--- lib/src/constants/environments.dart
+++ lib/src/constants/environments.dart
@@ -1,6 +1,7 @@
 class _Environments {
-  // final api = 'https://nest-auth.ubbly.club/api';
-  final api = 'http://localhost:3000';
+  final api = 'https://nest-auth.ubbly.club/api';
+
+  // final api = 'http://localhost:3000';
 }
 
 _Environments environments = _Environments();
