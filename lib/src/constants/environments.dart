917e4148baee61df54851c413b83846f782d5b76
Denzel Code <denzelcodedev@gmail.com>
2021-07-19 22:39:48 -0400

    refactor: folder structure

--- lib/src/constants/environments.dart
+++ lib/src/constants/environments.dart
@@ -1,5 +1,6 @@
 class _Environments {
-  final api = 'https://nest-auth.ubbly.club/api';
+  // final api = 'https://nest-auth.ubbly.club/api';
+  final api = 'http://localhost:3000';
 }
 
 _Environments environments = _Environments();
