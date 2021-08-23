0d26417fff2a0fa0e4be2bb355213ec229d8dec2
Denzel Code <denzelcodedev@gmail.com>
2021-08-23 01:13:16 -0400

    refactor: folder structure

--- lib/src/features/auth/models/user.dart
+++ lib/src/features/auth/models/user.dart
@@ -2,16 +2,19 @@ class User {
   late String id;
   late String username;
   late String email;
+  late bool online;
 
   User({
     required this.id,
     required this.username,
     required this.email,
+    required this.online,
   });
 
   User.fromJson(Map<String, dynamic> json) {
     id = json['_id'];
     username = json['username'];
     email = json['email'];
+    online = json['online'];
   }
 }
