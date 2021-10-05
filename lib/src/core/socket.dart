a0b45f46d7867490e70164d25cab27a3f17d4b80
Denzel Code <denzelcodedev@gmail.com>
2021-10-05 00:31:07 -0400

    fix: socket connection working

--- lib/src/core/socket.dart
+++ lib/src/core/socket.dart
@@ -1,13 +1,19 @@
 import 'package:auth/src/app.dart';
 import 'package:auth/src/constants/environments.dart';
 import 'package:auth/src/features/auth/logic/repository/auth_repository.dart';
+import 'package:auth/src/shared/views/widgets/dialog/alert_dialog_widget.dart';
+import 'package:flutter/material.dart';
 import 'package:socket_io_client/socket_io_client.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SocketConnection {
   static Socket socket = io(
     environments.socket,
-    OptionBuilder().disableAutoConnect().setExtraHeaders({}).build(),
+    OptionBuilder()
+        .setTransports(['websocket'])
+        .disableAutoConnect()
+        .setExtraHeaders({})
+        .build(),
   );
 
   static Future<Socket> init() async {
@@ -28,6 +34,22 @@ class SocketConnection {
     socket.io.options['extraHeaders'] = {
       'Authorization': 'Bearer $accessToken'
     };
+
+    print('pwewererwefge');
+
+    socket.onError((error) {
+      if (context == null) {
+        return;
+      }
+
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialogWidget(
+          title: error['error'] ?? error['message'],
+          description: error['message'],
+        ),
+      );
+    });
 
     socket.onConnect((_) => socket.emit('user:subscribe'));
 
