025c8915741c3d4a0e3230615524deb17af8572c
Denzel Code <denzelcodedev@gmail.com>
2021-10-18 05:15:56 -0400

    feat: initial notifications implementation

--- lib/src/features/notification/logic/provider/subscription_api_provider.dart
+++ lib/src/features/notification/logic/provider/subscription_api_provider.dart
@@ -1,4 +1,6 @@
+import 'package:auth/src/features/auth/logic/interceptors/auth_token_interceptor.dart';
 import 'package:auth/src/shared/logic/http/api.dart';
+import 'package:auth/src/shared/logic/http/interceptors/error_dialog_interceptor.dart';
 
 class SubscriptionAPIProvider {
   Future<void> registerSubscription(String token) async {
@@ -6,6 +8,13 @@ class SubscriptionAPIProvider {
   }
 
   Future<void> deleteSubscription(String token) async {
-    await api.delete('/subscription/mobile', data: {'subscription': token});
+    await api.delete(
+      '/subscription/mobile',
+      data: {'subscription': token},
+      options: Options(headers: {
+        AuthTokenInterceptor.skipHeader: true,
+        ErrorDialogInterceptor.skipHeader: true,
+      }),
+    );
   }
 }
